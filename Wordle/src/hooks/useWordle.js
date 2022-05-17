ce2027e858110c47d93b0c5420f2e2233f0aabc4
Diwash <diwashdahal75@gmail.com>
2022-05-17 20:00:01 +0545

    add wordle

---
+++ Wordle/src/hooks/useWordle.js
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+const useWordle = (solution) => {
+  const [turn, setTurn] = useState(0);
+  const [currentGuess, setCurrentGuess] = useState("");
+  const [guesses, setGuesses] = useState([]);
+  const [history, setHistory] = useState([]);
+  const [isCorrect, SetIsCorrect] = useState(false);
+
+  const formatGuess = () => {};
+
+  const addNewGuess = () => {};
+
+  const handleKeyup = () => {
+      
+  };
+
+  return { turn, currentGuess, guesses, isCorrect, handleKeyup };
+};
+
+export default useWordle;
